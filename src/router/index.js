7b3e89486e4e2a8167bf2960c36c45e7a53671c7
XinYou <aas7624328@gmail.com>
2026-09-14 09:19:42 +0800

    aaa

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { useAuthStore } from "@/stores/auth.store";
-import { createRouter, createWebHistory } from 'vue-router'
 
 
 const routes = [
